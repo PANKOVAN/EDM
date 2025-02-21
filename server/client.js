d8488444ad750e4341d962e7f3f856cb557a7283
pan <pankovan@yandex.ru>
2025-02-21 19:06:25 +0300

    EMBEDDED

--- server/client.js
+++ server/client.js
@@ -90,31 +90,173 @@ class ClientHelpers {
         else if (obj) return preffix + obj.toString() + suffix;
         return '';
     }
-    static setEmbeddedData(str, o) {
-        try {
-            let s = '{{' + JSON.stringify(o) + '}}';
-            let i = str.indexOf('{{');
-            let j = str.lastIndexOf('}}');
 
-            if (i >= 0 && j > i) str = str.substring(0, i) + s + str.substring(j + 2)
-            else str = str + '\t' + s;
+    /**
+     * 
+     * @param {string} source исходная строка в которую предполагется внедрить данные
+     * @param {*} name имя внедренных данных (необязательно)
+     * @param {*} obj объект данных
+     * @param {*} scheme схема вида :
+     *                              {
+     *                                   <имя свойства>: <тип данных>,
+     *                                   <имя свойства>: <тип данных>,
+     * 
+     *                                   <имя свойства>: <тип данных>,
+     *                              }
+     */
 
-            return str;
-        } catch { }
-        return '{{}}'
+
+    static embeddedTypes = {
+        BigInt64: 8,
+        BigUint64: 8,
+        Float32: 4,
+        Float64: 8,
+        Int16: 2,
+        Int32: 4,
+        Int8: 1,
+        Uint16: 2,
+        Uint32: 4,
+        Uint8: 1,
     }
-    static getEmbeddedData(str) {
-        try {
-            let i = str.indexOf('{{');
-            let j = str.lastIndexOf('}}');
+    static setEmbeddedData(source, name, obj, scheme) {
+        source = source || '';
+        name = name || '';
+        obj = obj || {};
+        scheme = scheme || {};
 
-            if (i >= 0 && j > i) str = str.substring(i + 2, j);
-            else str = '';
+        // Длина внедряемых данных
+        let l = 0;
+        for (let n in scheme) {
+            let t = scheme[n];
+            let l1 = ClientHelpers.embeddedTypes[t];
+            if (!l1) throw new Error(`Тип данных ${n}:${t} задан неправильно!!!`);
+            l += l1;
+        }
 
-            return JSON.parse(str);
-        } catch { }
-        return undefined
+
+        // Положить все в буфер
+        let a = new Uint8Array(l);
+        let d = new DataView(a.buffer);
+        let offset = 0;
+        for (let n in scheme) {
+            let t = scheme[n];
+            d[`set${t}`](offset, obj[n]);
+            offset += ClientHelpers.embeddedTypes[t];
+        }
+
+        // Строка для внедрения
+        let s = btoa(String.fromCharCode(...a));
+
+        // Внедрить строку в исходную
+        let i = source.indexOf(`{{${name}=`);
+        let j = source.indexOf(`}}`, i);
+        s = `{{${name}=${s}}}`;
+
+        if (i >= 0 && j > i) source = source.substring(0, i) + s + source.substring(j + 2);
+        else source = source.trimEnd() + ' ' + s + '\n';
+
+        return source;
     }
+    static getEmbeddedData(source, name, obj, scheme) {
+        source = source || '';
+        name = name || '';
+        obj = obj || {};
+        scheme = scheme || {};
+
+        // Длина внедряемых данных
+        let l = 0;
+        for (let n in scheme) {
+            let t = scheme[n];
+            let l1 = ClientHelpers.embeddedTypes[t];
+            if (!l1) throw new Error(`Тип данных ${n} не определен`);
+            l += l1;
+        }
+
+        // Прочитать данные в объект
+        let i = source.indexOf(`{{${name}=`);
+        let j = source.indexOf(`}}`, i);
+        if (i >= 0 && j > i) {
+            let s = source.substring(i + 2 + name.length + 1, j);
+            let a = Uint8Array.from(atob(s), c => c.charCodeAt(0));
+            if (a.byteLength == l) {
+                let d = new DataView(a.buffer);
+                let offset = 0;
+                for (let n in scheme) {
+                    let t = scheme[n];
+                    obj[n] = d[`get${t}`](offset);
+                    offset += ClientHelpers.embeddedTypes[t];
+                }
+            }
+        }
+
+        return obj;
+    }
+    static testEmbeddedData(source, name) {
+        let i = source.indexOf(`{{${name}=`);
+        let j = source.indexOf(`}}`, i);
+        return (i >= 0 && j > i)
+    }
+    //     static setEmbeddedData(str, o) {
+    //         if (!o) o = {};
+    //         else if (typeof o != 'object' || Array.isArray(o)) o = { _: o };
+    //         try {
+    //             let s = '{' + JSON.stringify(o) + '}';
+    //             let i = str.indexOf('{{');
+    //             let j = str.lastIndexOf('}}');
+
+    //             if (i >= 0 && j > i) str = str.substring(0, i) + s + str.substring(j + 2)
+    //             else str = str + '\t' + s;
+
+    //             return str;
+    //         } catch { }
+    //         return '{{}}'
+    //     }
+    //     static getEmbeddedData(str) {
+    //         /*
+    //         try {
+    //             debugger;
+    //             let a = new Float32Array([
+    //                 0.123456789,
+    //                 0.123456789,
+    //                 0.123456789,
+    //                 0.123456789,
+    //                 0.123456789,
+    //                 12345.123456789,
+    //                 12345.123456789,
+    //                 12345.123456789,
+    //                 12345.123456789,
+    //                 12345.123456789,
+    //             ]);
+    //             console.log(JSON.stringify(a));
+    //             let b = a.buffer;
+    //             console.log(b.byteLength);
+
+    //             let s = btoa(String.fromCharCode(...new Uint8Array(b)));
+    //             console.log(s);
+
+    //             let u = Uint8Array.from(atob(s), c => c.charCodeAt(0));
+    //             let a1 = new Float32Array(u.buffer);
+    //             console.log(JSON.stringify(a1));
+
+    //         }
+    //         catch (e) {
+    //             console.log(e);
+    //             debugger;
+    //         }
+    // */
+
+
+    //         try {
+    //             let i = str.indexOf('{{');
+    //             let j = str.lastIndexOf('}}');
+
+    //             if (i >= 0 && j > i) str = str.substring(i + 1, j + 1);
+    //             else str = {};
+
+    //             return JSON.parse(str);
+    //         } catch { }
+    //         return {}
+    //     }
 
 }
 module.exports = ClientHelpers
